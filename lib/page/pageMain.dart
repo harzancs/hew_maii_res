c82a206802610ceceed3329c013980adf8e353e3
harzancs <592021212@tsu.ac.th>
2019-12-31 22:08:49 +0700

    zan 31122 2208

--- lib/page/pageMain.dart
+++ lib/page/pageMain.dart
@@ -7,51 +7,163 @@ class PageMain extends StatefulWidget {
 }
 
 class _PageMainState extends State<PageMain> {
+  TextStyle listurgerBar =
+      new TextStyle(fontFamily: FontStyles().fontFamily, fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 1000,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/bg/bg_1.jpg'), fit: BoxFit.cover),
+      appBar: AppBar(
+        iconTheme: new IconThemeData(color: Color(0xFFFF6F18)),
+        backgroundColor: Colors.white,
+        title: Text(
+          "จัดการร้านค้า",
+          style: TextStyle(
+              fontFamily: FontStyles().fontFamily, color: Color(0xFFFF6F18)),
         ),
-        child: Container(
-            // color: Colors.blueAccent,
-            width: 900,
+      ),
+      drawer: Drawer(
+        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              "ครัวอินดี้",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: FontStyles().fontFamily,
+                fontSize: 18,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 5.0,
+                    color: Color(0xFFFF6F18),
+                  ),
+                  Shadow(
+                    offset: Offset(6.0, 6.0),
+                    blurRadius: 8.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/bg/bg_1.jpg'), fit: BoxFit.cover),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.account_box,
+              color: Color(0xFFFF6F18),
+              size: 30,
+            ),
+            title: Text(
+              'ข้อมูลร้าน',
+              style: listurgerBar,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.assignment,
+              color: Color(0xFFFF6F18),
+              size: 30,
+            ),
+            title: Text(
+              'รายการอาหาร',
+              style: listurgerBar,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.assessment,
+              color: Color(0xFFFF6F18),
+              size: 30,
+            ),
+            title: Text(
+              'ยอดขายรายวัน',
+              style: listurgerBar,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          Divider(
+            color: Color(0xFFFF6F18),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.power_settings_new,
+              color: Color(0xFFFF6F18),
+              size: 30,
+            ),
+            title: Text(
+              'ออกจากระบบ (Sing Out)',
+              style: listurgerBar,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ]),
+      ),
+      body: Container(
+          height: 1000,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/bg/bg_1.jpg'), fit: BoxFit.cover),
+          ),
+          child: Container(
             child: Column(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(15),
                 ),
-                Text("data"),
-                Text("data1"),
-                Icon(Icons.add_a_photo),
-                Container(
-                  color: Colors.brown,
-                  height: 100,
-                  width: 250,
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        "data",
-                        style: TextStyle(
-                            color: Color(0xFFFF1D41D),
-                            fontFamily: FontStyles().fontFamily,fontSize: 25),
-                            
-                      ),
-                      Text(
-                        "data1",
-                        style: TextStyle(
-                            color: Color(0xFFFF1D41D),
-                            fontFamily: FontStyles().fontFamily),
-                      ),
-                      Icon(Icons.add_a_photo)
-                    ],
-                  ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      " จัดการร้านค้า",
+                      style: TextStyle(
+                          fontFamily: FontStyles().fontFamily,
+                          fontSize: 26,
+                          color: Colors.white),
+                    )
+                  ],
                 )
               ],
-            )),
+            ),
+          )),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 60.0,
+              width: 5.0,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              color: Color(0xFFFF6F18),
+              child: Text(
+                "ยืนยันรายการ",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: FontStyles().fontFamily,
+                    fontSize: 16),
+              ),
+            ),
+            Container(
+              height: 60.0,
+              width: 5.0,
+            ),
+          ],
+        ),
+        color: Colors.white,
+        notchMargin: 8.0,
       ),
     );
   }
